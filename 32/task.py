62f53c7039cccd6a98d152d3e83db18120c4ea89
IrinaSavosh <irina_savosh@mail.ru>
2023-02-16 21:25:20 +0300

    Task 32

--- 32/task.py
+++ 32/task.py
@@ -8,3 +8,20 @@
 # 0, -5, -5, 7]
 # Вывод: [1, 9, 13, 14, 19]
 
+import random
+a = int(input('Введите минимальное число диапазона(не иенее 0): '))
+b = int(input('Введите максимальное число диапазона (не более 100): '))
+n = random.randint(10, 15)
+arr=[random.randint(0,100) for i in range(n)]
+print(arr)
+
+
+def define_indexes(lst, min_value, max_value):
+   new_lst = []
+   for i in range(len(lst)):
+      if lst[i] > min_value and lst[i] < max_value:
+         new_lst.append(i)
+   return new_lst
+
+new_lst = define_indexes(arr, a, b)
+print(new_lst)
