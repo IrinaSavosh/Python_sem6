4a14de93df9c2c68108a51ab51312e62ef289799
IrinaSavosh <irina_savosh@mail.ru>
2023-02-16 15:19:08 +0300

    Task 30

--- 30/task.py
+++ 30/task.py
@@ -6,3 +6,23 @@
 
 # Ввод: 7 2 5
 # Вывод: 7 9 11 13 15
+
+n = int(input('Введите начальное число: '))
+m = int(input('Введите шаг прогрессии: '))
+l = int(input('Введите количество элементов: '))
+
+
+def arithmetic_progression(number, step, amount):
+   lst = []
+   for i in range(1, amount+1):
+      lst.append(number + (i-1) * step)
+   return lst
+
+def show_element_from_new_line(arr):
+   for i in range(len(arr)):
+      print(arr[i])
+
+
+lst1 = arithmetic_progression(n, m, l)
+print(lst1)
+show_element_from_new_line(lst1)
